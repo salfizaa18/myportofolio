e54d9b37b0a723ab992c31756887d99f7d8411f3
Salwa <salwa.hafiza@ui.ac.id>
2026-09-14 15:26:46 +0700

    update experience

--- main/models.py
+++ main/models.py
@@ -16,8 +16,8 @@ class Experience(models.Model):
     description = models.TextField()
     category = models.CharField(max_length=20, choices=EXPERIENCE_CHOICES, default='full-time')
     thumbnail = models.URLField(blank=True, null=True)
-    started_at = models.IntegerField()
-    ended_at = models.IntegerField(blank=True, null=True)
+    started_at = models.DateTimeField(auto_now_add=True)
+    ended_at = models.DateTimeField(blank=True, null=True)
     def __str__(self):
         return self.title
     
