082f674bf3e6b3a6d23c23c5f451b63a4ee96d8f
Salwa <salwa.hafiza@ui.ac.id>
2026-09-20 21:27:26 +0700

    Add education routes

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from main.views import show_main, show_experience, show_education, create_project, show_projects, get_projects_json, delete_project
+from main.views import show_main, show_experience, show_education, create_project, show_projects, get_projects_json, delete_project, create_education, update_education, delete_education, get_education_json
 
 
 app_name = "main"
@@ -10,6 +10,10 @@ urlpatterns = [
     path("", show_main, name="show_main"),
     path("experience/", show_experience, name="show_experience"),
     path("education/", show_education, name="show_education"),
+    path("education/add/", create_education, name="create_education"),
+    path("education/<uuid:education_id>/edit/", update_education, name="update_education"),
+    path("education/<uuid:education_id>/delete/", delete_education, name="delete_education"),
+    path("api/education/", get_education_json, name="get_education_json"),
     path("projects/add/", create_project, name="create_project"),
     path("projects/", show_projects, name="show_projects"),
     path("api/projects/", get_projects_json, name="get_projects_json"),
